f9dbbdeb7f1d9b6a2fc6661f045e11ee5c02edbe
Vinicius Pontes <vpontessilva@gmail.com>
2019-12-17 01:05:43 -0300

    feat(project) Created initial structure

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Home from './routes/home'
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={Home} />
+        </Switch>
+    </BrowserRouter>
+    , document.getElementById('root'));
+
